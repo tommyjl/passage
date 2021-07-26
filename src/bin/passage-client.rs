1dc9aea0bb994214a01d004741953fabff83a463
Tommy Joe Lund <tomlun92@gmail.com>
2021-07-26 19:07:07 +0200

    Add a client

--- src/bin/passage-client.rs
+++ src/bin/passage-client.rs
@@ -1,3 +1,34 @@
+use clap::Clap;
+use passage::client::Client;
+
+#[derive(Clap)]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    Get { key: String },
+    Set { key: String, value: String },
+    Remove { key: String },
+}
+
 fn main() {
-    println!("Client: Hello world!");
+    let opts = Opts::parse();
+    let mut client = Client::new("127.0.0.1:12345");
+    match opts.subcmd {
+        SubCommand::Get { key } => {
+            let buf = client.get(key);
+            print!("{}", String::from_utf8(buf).unwrap());
+        }
+        SubCommand::Set { key, value } => {
+            let buf = client.set(key, value);
+            print!("{}", String::from_utf8(buf).unwrap());
+        }
+        SubCommand::Remove { key } => {
+            let buf = client.remove(key);
+            print!("{}", String::from_utf8(buf).unwrap());
+        }
+    };
 }
--- src/client.rs
+++ src/client.rs
@@ -1,1 +1,47 @@
-pub struct client;
+use crate::server::MESSAGE_MAX_SIZE;
+use socket2::{Domain, Socket, Type};
+use std::io::prelude::*;
+use std::net::SocketAddr;
+
+pub struct Client {
+    conn: Socket,
+}
+
+impl Client {
+    pub fn new(addr: &str) -> Self {
+        let addr: SocketAddr = addr.parse().unwrap();
+        let socket = Socket::new(Domain::IPV4, Type::STREAM, None).unwrap();
+        socket.connect(&addr.into()).unwrap();
+        Self { conn: socket }
+    }
+
+    pub fn get(&mut self, key: String) -> Vec<u8> {
+        let msg = format!("get {}\r\n", key);
+        self.conn.write(msg.as_bytes()).unwrap();
+
+        let mut buf = [0; MESSAGE_MAX_SIZE];
+        let len = self.conn.read(&mut buf).unwrap();
+
+        buf[0..len].to_vec()
+    }
+
+    pub fn set(&mut self, key: String, value: String) -> Vec<u8> {
+        let msg = format!("set {} {}\r\n", key, value);
+        self.conn.write(msg.as_bytes()).unwrap();
+
+        let mut buf = [0; MESSAGE_MAX_SIZE];
+        let len = self.conn.read(&mut buf).unwrap();
+
+        buf[0..len].to_vec()
+    }
+
+    pub fn remove(&mut self, key: String) -> Vec<u8> {
+        let msg = format!("remove {}\r\n", key);
+        self.conn.write(msg.as_bytes()).unwrap();
+
+        let mut buf = [0; MESSAGE_MAX_SIZE];
+        let len = self.conn.read(&mut buf).unwrap();
+
+        buf[0..len].to_vec()
+    }
+}
