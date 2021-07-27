d2b06e711f4d471e97c9e0a1f4e3753418c39809
Tommy Joe Lund <tomlun92@gmail.com>
2021-07-27 22:06:12 +0200

    Add a simple benchmark

---
+++ src/bin/passage-benchmark.rs
@@ -0,0 +1,65 @@
+use clap::Clap;
+use log::info;
+use passage::client::Client;
+use passage::default_env;
+use std::time::Instant;
+
+#[derive(Clap)]
+struct Opts {
+    #[clap(long, short, default_value = "1000")]
+    requests: i32,
+
+    #[clap(subcommand)]
+    kind: BenchKind,
+}
+
+#[derive(Clap)]
+enum BenchKind {
+    Get,
+    Set,
+}
+
+impl std::fmt::Display for BenchKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let kind = match self {
+            BenchKind::Get => "get",
+            BenchKind::Set => "set",
+        };
+        write!(f, "{}", kind)
+    }
+}
+
+fn bench_get() {
+    let mut client = Client::new("127.0.0.1:12345");
+    let _ = client.get("drink".to_string());
+}
+
+fn bench_set() {
+    let mut client = Client::new("127.0.0.1:12345");
+    let _ = client.set("drink".to_string(), "water".to_string());
+}
+
+fn main() {
+    default_env!("RUST_LOG", "trace");
+    env_logger::init();
+
+    let opts = Opts::parse();
+    let bench_fn = match opts.kind {
+        BenchKind::Get => bench_get,
+        BenchKind::Set => bench_set,
+    };
+
+    let time = Instant::now();
+    for _ in 0..opts.requests {
+        bench_fn();
+    }
+    let elapsed = time.elapsed().as_millis();
+
+    info!(
+        "{} {} requests took {} ms",
+        opts.requests, opts.kind, elapsed
+    );
+
+    let avg = (elapsed as f64) / (opts.requests as f64);
+    info!("Average: {} ms", avg);
+}
