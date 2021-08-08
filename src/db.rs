af79ef80b1ac6178c2347c71aa43a462f39922f6
Tommy Joe Lund <tomlun92@gmail.com>
2021-08-08 23:55:18 +0200

    Add error type in db

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,42 @@
 use crate::command::Command;
 use crate::object::Object;
 use std::collections::HashMap;
-use std::sync::RwLock;
+use std::fmt::Display;
+use std::sync::{PoisonError, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 pub trait Database: Send + Sync {
-    fn execute(&self, cmd: Command) -> Result<Object, ()>;
+    fn execute(&self, cmd: Command) -> DbResult<Object>;
+}
+
+pub type DbResult<'a, T> = Result<T, DbError<'a>>;
+
+#[derive(Debug)]
+pub enum DbError<'a> {
+    NotFound,
+    ReadLock(PoisonError<RwLockReadGuard<'a, HashMap<Vec<u8>, Object>>>),
+    WriteLock(PoisonError<RwLockWriteGuard<'a, HashMap<Vec<u8>, Object>>>),
+}
+
+impl<'a> Display for DbError<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DbError::NotFound => write!(f, "Database entry was not found"),
+            DbError::ReadLock(inner) => write!(f, "{}", inner),
+            DbError::WriteLock(inner) => write!(f, "{}", inner),
+        }
+    }
+}
+
+impl<'a> From<PoisonError<RwLockReadGuard<'a, HashMap<Vec<u8>, Object>>>> for DbError<'a> {
+    fn from(err: PoisonError<RwLockReadGuard<'a, HashMap<Vec<u8>, Object>>>) -> Self {
+        DbError::ReadLock(err)
+    }
+}
+
+impl<'a> From<PoisonError<RwLockWriteGuard<'a, HashMap<Vec<u8>, Object>>>> for DbError<'a> {
+    fn from(err: PoisonError<RwLockWriteGuard<'a, HashMap<Vec<u8>, Object>>>) -> Self {
+        DbError::WriteLock(err)
+    }
 }
 
 pub struct HashMapDatabase {
@@ -18,21 +50,21 @@ impl HashMapDatabase {
         }
     }
 
-    fn get(&self, key: Vec<u8>) -> Result<Object, ()> {
-        self.db.read().unwrap().get(&key).cloned().ok_or(())
+    fn get(&self, key: Vec<u8>) -> DbResult<Object> {
+        self.db.read()?.get(&key).cloned().ok_or(DbError::NotFound)
     }
 
-    fn set(&self, key: Vec<u8>, value: Object) -> Result<Object, ()> {
-        self.db.write().unwrap().insert(key, value).ok_or(())
+    fn set(&self, key: Vec<u8>, value: Object) -> DbResult<Object> {
+        self.db.write()?.insert(key, value).ok_or(DbError::NotFound)
     }
 
-    fn remove(&self, key: Vec<u8>) -> Result<Object, ()> {
-        self.db.write().unwrap().remove(&key).ok_or(())
+    fn remove(&self, key: Vec<u8>) -> DbResult<Object> {
+        self.db.write()?.remove(&key).ok_or(DbError::NotFound)
     }
 }
 
 impl Database for HashMapDatabase {
-    fn execute(&self, cmd: Command) -> Result<Object, ()> {
+    fn execute(&self, cmd: Command) -> DbResult<Object> {
         match cmd {
             Command::Get(key) => self.get(key.into()),
             Command::Set(key, value) => self.set(key.into(), Object::BulkString(Some(value))),
